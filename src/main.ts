992be2eb12b268375d83f853c9c8cb6103913263
= <pumaronal2016@gmail.com>
2025-04-29 13:58:57 -0400

    Se agrego cambio para problema de Cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   
   // Configuración de CORS
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    // origin: ['http://localhost:5173'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
   });
